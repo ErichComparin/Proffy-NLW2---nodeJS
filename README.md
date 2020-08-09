78f6f0b8c6db457a8bb697f1674a4764ad5f716f
Erich Comparin <erich.comparin@gmail.com>
2020-08-09 15:17:43 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Projeto Proffy - API
+
+###### O projeto
+
+Projeto desenvolvido durante a Next Level Week #2 promovida pela @rocketseat.
+
+###### Descrição
+
+API criada em NodeJS.
---
+++ knexfile.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite3')
+  },
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations')
+  },
+  useNullAsDefault: true,
+};
+
+// Migrations: controlam a versão do banco de dados
